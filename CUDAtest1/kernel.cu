344e7c58c135554b8f1752d007dc156d695aaa23
jimmy hu <60071jimmy@gmail.com>
2017-10-07 12:41:22 +0800

    新增註解
    新增註解

--- CUDAtest1/kernel.cu
+++ CUDAtest1/kernel.cu
@@ -7,8 +7,8 @@
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{																		//	addKernel function, addKernel
+__global__ void addKernel(int *c, const int *a, const int *b)			//	addKernel¨ç¼Æ
+{																		//	addKernel function, addKernel¨ç¼Æ
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
