3898642ac1a1dfd17bc21f339bd542f94a82aa4f
jimmy hu <60071jimmy@gmail.com>
2017-10-09 14:17:11 +0800

    新增註解
    新增註解

--- CUDAEncryptionTool/kernel.cu
+++ CUDAEncryptionTool/kernel.cu
@@ -1,5 +1,5 @@
 //-----include header files, 引入標頭檔-----
-#include "cuda_runtime.h"
+#include "cuda_runtime.h"												//	include "cuda_runtime.h" header file, 引入標頭檔cuda_runtime.h
 #include "device_launch_parameters.h"									//	include "device_launch_parameters.h" header file, 引入標頭檔device_launch_parameters.h
 
 #include <stdio.h>														//	include "stdio.h" header file, 引入標頭檔stdio.h
