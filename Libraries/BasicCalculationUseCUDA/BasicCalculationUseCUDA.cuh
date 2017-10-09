c0350b1067fda41cba41ba14d222828414b9d2e3
jimmy hu <60071jimmy@gmail.com>
2017-10-09 20:16:07 +0800

    Add, Subtract, Multiply, Divide funtion
    Add, Subtract, Multiply, Divide funtion

--- Libraries/BasicCalculationUseCUDA/BasicCalculationUseCUDA.cuh
+++ Libraries/BasicCalculationUseCUDA/BasicCalculationUseCUDA.cuh
@@ -7,8 +7,17 @@
 #include "device_launch_parameters.h"									//	include "device_launch_parameters.h" header file, 引入標頭檔device_launch_parameters.h
 
 //-----funtion declaration, 副程式宣告區----- 
+//	-----Add, Subtract, Multiply, Divide funtion declaration, 加法、減法、乘法、除法函數宣告
 __global__ void addKernel(int *c, const int *a, const int *b);
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void subKernel(int *c, const int *a, const int *b);
 cudaError_t subWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void mulKernel(int *c, const int *a, const int *b);
+cudaError_t mulWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void divKernel(int *c, const int *a, const int *b);
+cudaError_t divWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+//	-----
