9d0808f9f6b53b36a79f5052c3f31731f3fc6611
jimmy hu <60071jimmy@gmail.com>
2017-10-09 16:53:08 +0800

    撰寫CUDA基本運算函數
    撰寫CUDA基本運算函數

--- CUDAEncryptionTool/BasicCalculationUseCUDA.cu
+++ CUDAEncryptionTool/BasicCalculationUseCUDA.cu
@@ -1,2 +1,12 @@
 /*	Basic Calculation Use CUDA，使用CUDA執行基本運算	*/
 
+//-----include header files, 引入標頭檔-----
+#include "cuda_runtime.h"												//	include "cuda_runtime.h" header file, 引入標頭檔cuda_runtime.h
+#include "device_launch_parameters.h"									//	include "device_launch_parameters.h" header file, 引入標頭檔device_launch_parameters.h
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
+}
+
