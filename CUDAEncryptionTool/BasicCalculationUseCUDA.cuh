ec359a3cba9ce598cca5c75267d8b984b4349b59
jimmy hu <60071jimmy@gmail.com>
2017-10-09 16:50:27 +0800

    建立基本運算函數
    建立基本運算函數

---
+++ CUDAEncryptionTool/BasicCalculationUseCUDA.cuh
@@ -0,0 +1,9 @@
+/*	Basic Calculation Use CUDA，使用CUDA執行基本運算	*/
+
+//-----include header files, 引入標頭檔-----
+#include "cuda_runtime.h"												//	include "cuda_runtime.h" header file, 引入標頭檔cuda_runtime.h
+#include "device_launch_parameters.h"									//	include "device_launch_parameters.h" header file, 引入標頭檔device_launch_parameters.h
+
+//-----funtion declaration, 副程式宣告區----- 
+__global__ void addKernel(int *c, const int *a, const int *b);
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
