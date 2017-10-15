237b3bc28a87fe200717af131c79935e4ca28b2c
jimmy hu <60071jimmy@gmail.com>
2017-10-15 16:34:54 +0800

    新增註解
    新增註解

--- Libraries/BasicCalculationUseCUDA/BasicCalculationUseCUDA.cuh
+++ Libraries/BasicCalculationUseCUDA/BasicCalculationUseCUDA.cuh
@@ -21,7 +21,7 @@ __global__ void divKernel(int *c, const int *a, const int *b);			//	宣告divKerne
 cudaError_t divWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 //	-----Bitwise operation function declaration, 位元運算函數宣告
-__global__ void orKernel(int *c, const int *a, const int *b);
+__global__ void orKernel(int *c, const int *a, const int *b);			//	宣告orKernel函數
 cudaError_t orWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void andKernel(int *c, const int *a, const int *b);
