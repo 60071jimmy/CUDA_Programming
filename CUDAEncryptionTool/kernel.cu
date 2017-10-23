a049e63b201e6a525e627130e720835cd11a30d4
jimmy hu <60071jimmy@gmail.com>
2017-10-23 17:43:04 +0800

    新增註解
    新增註解

--- CUDAEncryptionTool/kernel.cu
+++ CUDAEncryptionTool/kernel.cu
@@ -25,8 +25,8 @@ int main()																//	main function, 主程式
 
 	cudaStatus = addWithCuda(c, a, b, arraySize);
     if (cudaStatus != cudaSuccess) {									//	若cudaStatus為cudaSuccess
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
+        fprintf(stderr, "addWithCuda failed!");							//	顯示錯誤訊息
+        return 1;														//	回傳1並結束程式
     }
 
     printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
