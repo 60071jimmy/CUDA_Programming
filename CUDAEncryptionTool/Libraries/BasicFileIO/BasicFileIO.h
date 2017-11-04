7c158245b0ec31c0ac775e814755868cede34e4c
jimmy hu <60071jimmy@gmail.com>
2017-11-04 18:47:43 +0800

    Add comment
    Add comment

--- CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.h
+++ CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.h
@@ -6,7 +6,7 @@
  */
 //-----引入標頭檔-----
 #include <math.h>														//	include math.h header file, 引入標頭檔math.h
-#include <stdbool.h>													//	引入標頭檔stdbool.h
+#include <stdbool.h>													//	include stdbool.h header file, 引入標頭檔stdbool.h
 #include <stdio.h>														//	引入標頭檔stdio.h
 #include <stdlib.h>														//	引入標頭檔stdlib.h
 #include <string.h>														//	引入標頭檔string.h
