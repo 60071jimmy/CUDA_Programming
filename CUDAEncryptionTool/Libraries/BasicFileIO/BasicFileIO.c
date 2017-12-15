9a00f6bb3e65d8027f1b0d72c6d44af4c67fc718
jimmy hu <60071jimmy@gmail.com>
2017-12-15 21:55:56 +0800

    Add comment
    Add comment

--- CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.c
+++ CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.c
@@ -42,7 +42,7 @@ bool FileWritePermissionCheck(const char *file_name)					//	FileWritePermissionC
 	} 																	//	ending if statement, 結束if敘述 
 	else 																//	if the file which path is file_name isn't readable, 若檔案不可讀取 
 	{																	//	entering else statement, 進入else敘述 
-	    return false;													//	回傳false 
+	    return false;													//	return false, 回傳false 
 	}																	//	結束else敘述 
 } 																		//	結束FileWritePermissionCheck(檔案寫入權限檢查)副程式
 
