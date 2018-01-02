c291bff48f653fd429637f64e2509da55d07f9da
jimmy hu <60071jimmy@gmail.com>
2018-01-02 23:31:04 +0800

    Add comment
    Add comment

--- CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.c
+++ CUDAEncryptionTool/Libraries/BasicFileIO/BasicFileIO.c
@@ -70,7 +70,7 @@ bool FileWrite(const char *file_name,const char *input_str,const char *mode)
 		{																//	entering if statement, 進入if敘述
 			printf("FileWrite:permission ERROR!\n");					//	顯示"FileWrite:permission ERROR!"並換行
 			return false;												//	return false and exit funtion, 回傳false並結束副程式返回
-		}																//	結束if敘述
+		}																//	ending if statement, 結束if敘述
 	}																	//	結束if敘述
 	file_point = fopen(file_name, mode);								//	以mode模式打開檔案
 	//	fprintf或fputs語法二擇一使用
